7abdd0f2f27873e137befcd8f5ef4a326e587ac9
StaveYt <stavex112yt@gmail.com>
2023-02-22 15:06:31 +0100

    pocinje uređivanje indexa, funkcionalan log in

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
-
-let fullData = []
+let fullData = [];
 
 var requestOptions = {
   method: 'GET',
@@ -17,5 +16,107 @@ function parseData(data){
     if (row!=""){fullData.push(row.split(',').slice(1))}
   })
 
-  console.log(fullData)
+  console.log(fullData);
+}
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC53RtA4CcnEAzVIpNJq0jCoUkGSD8Uwtw",
+  authDomain: "editnogonet.firebaseapp.com",
+  databaseURL: "https://editnogonet-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "editnogonet",
+  storageBucket: "editnogonet.appspot.com",
+  messagingSenderId: "411313607141",
+  appId: "1:411313607141:web:eca032267a53c7198c1f0c",
+  measurementId: "G-N51E01Y79Y"
+};
+
+// Initialize Firebase
+const app = firebase.initializeApp(firebaseConfig);
+let database = app.firestore();
+
+let signInButton = document.getElementById("signInButton");
+
+signInButton.addEventListener("click", SignIn);
+
+let createAccountButton = document.getElementById('createAccount');
+createAccountButton.addEventListener('click', ChangeToSignUp);
+
+
+
+function ChangeToSignUp(){
+
+  let emailWrapper = document.getElementById('emailWrapper')
+  emailWrapper.classList.toggle('hidden');
+
+  let passConfirmWrapper = document.getElementById('passConfirmWrapper')
+  passConfirmWrapper.classList.toggle('hidden');
+
+  createAccountButton.removeEventListener('click', ChangeToSignUp)
+  createAccountButton.addEventListener('click', ChangeToSignIn)
+  createAccountButton.innerText = "Already have an account? Log in";
+
+ signInButton.innerText = "Register";
+ signInButton.removeEventListener('click', SignIn);
+ signInButton.addEventListener('click', SignUp);
+}
+
+function ChangeToSignIn(){
+  let signInContainer = document.getElementById('signInContainer');
+
+  let emailWrapper = document.getElementById('emailWrapper')
+  emailWrapper.classList.toggle('hidden');
+
+  let passConfirmWrapper = document.getElementById('passConfirmWrapper')
+  passConfirmWrapper.classList.toggle('hidden');
+
+  createAccountButton.removeEventListener('click', ChangeToSignIn);
+  createAccountButton.addEventListener('click', ChangeToSignUp);
+  createAccountButton.innerText = "Create an account";
+
+  signInButton.innerText = "Sign in";
+  signInButton.removeEventListener('click', SignUp);
+  signInButton.addEventListener('click', SignIn);
+}
+
+function SignIn(){
+  let username = document.getElementById('usernameInput').value;
+  let password = document.getElementById('passwordInput').value;
+  
+  database.collection("users").where("username", "==", username).where("password", "==", password).get().then((querySnapshot) => {
+    querySnapshot.forEach((doc) => {
+
+      console.log(`tocna šifra`);
+      document.getElementsByTagName('body')[0].removeChild(document.getElementById('signInContainer'))
+      });
+  });
+}
+
+function SignUp(){
+  let usernameTaken = false;
+
+  let username = document.getElementById('usernameInput').value;
+  let password = document.getElementById('passwordInput').value;
+  let passConfirm = document.getElementById('passConfirmInput').value;
+  let email = document.getElementById('emailInput').value;
+
+  database.collection("users").where("username", "==", username).get().then((querySnapshot) => {
+    if (querySnapshot.length != 0){usernameTaken = true}
+  });
+
+  if (usernameTaken){
+    alert('USERNAME TAKEN');
+  } else if (password != passConfirm){
+    alert('PASSWORDS NOT MATCHING');
+  } else{
+    database.collection("users").get().then((querySnapshot) => {
+      database.collection("users").add({
+        username: username,
+        password: password,
+        email: email,
+      })
+      
+      console.log("uspješna registracija");
+      document.getElementsByTagName('body')[0].removeChild(document.getElementById('signInContainer'));
+    });
+  }
 }
