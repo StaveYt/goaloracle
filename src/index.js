7a95ea40b075c85995483b85c64296096cd8d7d7
StaveYt <stavex112yt@gmail.com>
2023-02-23 22:01:26 +0100

    uredenje izgleda stranice kada korisnik nije ulogiran

--- src/index.js
+++ src/index.js
@@ -41,7 +41,10 @@ signInButton.addEventListener("click", SignIn);
 let createAccountButton = document.getElementById('createAccount');
 createAccountButton.addEventListener('click', ChangeToSignUp);
 
-
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`)
 
 function ChangeToSignUp(){
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./*.{html,js}"],
+  content: ["./*.{html,js}",
+  "./index.html",
+  "./html/**/*.{html,js}",
+  "./src/**/*.{html,js}"
+],
   theme: {
-    extend: {},
+    extend: {
+      boxShadow: {
+       'test': '2px 3px 8px #eee',
+      }
+    },
   },
   plugins: [],
 }
