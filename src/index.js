6fe2170f5ff28810bf7048791244c2d18e34459f
StavexYt <stavex112yt@gmail.com>
2023-04-28 14:10:10 +0200

    ranking

--- src/index.js
+++ src/index.js
@@ -60,6 +60,80 @@ function LoadInfo() {
 <td>90+12</td>
 <td>Feb 6, 2023.</td>
 </tr>  */}
+function SwitchNav(){
+  let navbarBig = document.getElementById('navlg')
+  if(user!=null){
+    navbarBig.innerHTML +=`
+    <div class="text-center">
+    <button
+      onclick="SignOut('signin')"
+      class="bg-[#1bb198] border-t-0 border-y-4 border-teal-800 hover:bg-teal-700 text-black font-bold my-3 py-2 text-center w-20 rounded shadow-slate-900 shadow-md hover:shadow-teal-500/50"
+    >
+      Sign Out
+    </button>
+  </div>`
+  }else{
+    body.innerHTML+=`<div class="fixed top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] hidden" id="signInBox">
+    <div class="flex flex-col m-auto p-4 w-96 text-slate-300 bg-stone-800 rounded-md" id="signInContainer">
+        <div class="m-4">
+            <input class="shadow-md shadow-slate-900 w-full p-2 bg-stone-600 text-center rounded-md" type="text"
+                id="usernameInput" placeholder="Username">
+        </div>
+        <div class="m-4 hidden" id="emailWrapper">
+            <input class="shadow-md shadow-slate-900 w-full p-2 bg-stone-600 text-center rounded-md" type="email"
+                id="emailInput" placeholder="Email">
+        </div>
+        <div class="m-4">
+            <input class="shadow-md shadow-slate-900 w-full p-2 text-center bg-stone-600 rounded-md" type="password"
+                id="passwordInput" placeholder="Password">
+        </div>
+        <div class="m-4 hidden" id="passConfirmWrapper">
+            <input class="shadow-md shadow-slate-900 w-full p-2 bg-stone-600 text-center rounded-md" type="password"
+                id="passConfirmInput" placeholder="Confirm Password">
+        </div>
+        <div class="m-4 hidden text-center" id="leagueSelectWrapper">
+            <label for="leagueSelect" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Liga</label>
+            <select id="leagueSelect"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected hidden disabled>Izaberite nogometnu ligu</option>
+            </select>
+        </div>
+        <div class="m-4 hidden text-center" id="teamSelectWrapper">
+            <label for="teamSelect" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Tim</label>
+            <select id="teamSelect"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+                <option selected hidden disabled>Izaberite Tim</option>
+            </select>
+        </div>
+        <button
+            class="my-4 mx-5 bg-[#1bb198] p-3 rounded-md shadow-slate-900  border-t-0 transition duration-300 border-y-4 border-green-800 shadow-md  font-bold text-lg [text-shadow:0_3px_3px_#000] hover:bg-green-700 text-slate-200 hover:shadow-green-500/50"
+            id="signInButton">
+            Sign in
+        </button>
+        <a class="my-2 self-center underline hover:cursor-pointer transition duration-200 hover:text-slate-700 [text-shadow:0_3px_3px_#000]"
+            id="createAccount">Create an account</a>
+    </div>
+</div>
+<script src="../src/register.js"></script>`
+  navbarBig.innerHTML+=`  <div class="text-center">
+    <button
+      onclick="SignOut('signin')"
+      class="bg-[#4b4b4f] border-t-0 border-y-4 border-stone-800 hover:bg-stone-700 font-bold my-3 py-2 text-center w-20 rounded shadow-slate-900 shadow-md hover:shadow-stone-500/50"
+    >
+      Sign Up
+    </button>
+  </div>
+  <div class="text-center">
+    <button
+      onclick="SignOut('signin')"
+      class="text-black bg-[#1bb198] border-t-0 border-y-4 border-teal-800 hover:bg-teal-700 font-bold mb-3 py-2 text-center w-20 rounded shadow-slate-900 shadow-md hover:shadow-teal-500/50"
+    >
+      Sign In
+    </button>
+  </div>`
+  }
+
+}
 function LoadLastGames(teamid){
   let latestGamesTable = document.getElementById("latestGames");
   fetch(`https://api.sportmonks.com/v3/football/teams/${teamid}?api_token=${sportMonksToken}&include=latest;`).then(res => res.json()).then(data => {
--- src/matchups.js
+++ src/matchups.js
@@ -1,126 +1,155 @@
 const today = new Date();
-const tdd = String(today.getDate()).padStart(2, '0');
-const tdm = String(today.getMonth() + 1).padStart(2, '0'); //January is 0!
+const tdd = String(today.getDate()).padStart(2, "0");
+const tdm = String(today.getMonth() + 1).padStart(2, "0"); //January is 0!
 const tdy = today.getFullYear();
 const urlParams = new URLSearchParams(window.location.search);
-const data = urlParams.get('data');
+const data = urlParams.get("data");
 console.log(data); // prints "hello"
 
+const liveGamesContainer = document.getElementById("liveGames");
+const finishedGamesContainer = document.getElementById("finishedGames");
+const schedualedGamesContainer = document.getElementById("schedualedGames");
 
+let matchupList = [];
 
-const liveGamesContainer = document.getElementById('liveGames')
-const finishedGamesContainer = document.getElementById('finishedGames')
-const schedualedGamesContainer = document.getElementById('schedualedGames')
-
-let matchupList = []
-
-function OnLoad(){
-  if(data==null){
-    GetMatchups(tdy,tdm,tdd,1,'all')
-  }else{
-    GetMatchups(tdy,tdm,tdd,1,data)
-    document.getElementById("leagueSelect").value=`${data}`
+function OnLoad() {
+  if (data == null) {
+    GetMatchups(tdy, tdm, tdd, 1, "all");
+  } else {
+    GetMatchups(tdy, tdm, tdd, 1, data);
+    document.getElementById("leagueSelect").value = `${data}`;
   }
 }
 
-function GetMatchups(year,month,day,page,league) {
-  fetch(`https://api.sportmonks.com/v3/football/fixtures/date/${year}-${month}-${day}?api_token=${sportMonksToken}&page=${page}&per_page=50&include=scores;league;venue;statistics.type;participants;`).then(response => response.json()).then(data => {
-    matchupList.push(...data.data)
-    ParseMatchups(data.data, league);
-    if(data.pagination.has_more)
-    {GetMatchups(year,month,day,page+1)}
-    else{sessionStorage.setItem('SHOWNMATCHUPS', JSON.stringify(matchupList))}
-  });
+function GetMatchups(year, month, day, page, league) {
+  fetch(
+    `https://api.sportmonks.com/v3/football/fixtures/date/${year}-${month}-${day}?api_token=${sportMonksToken}&page=${page}&per_page=50&include=scores;league;venue;statistics.type;participants;`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      matchupList.push(...data.data);
+      ParseMatchups(data.data, league);
+      if (data.pagination.has_more) {
+        GetMatchups(year, month, day, page + 1);
+      } else {
+        sessionStorage.setItem("SHOWNMATCHUPS", JSON.stringify(matchupList));
+      }
+    });
 }
 
 function ParseMatchups(fixtures, league) {
-  fixtures.forEach(fixture => {
-    if(fixture.league_id==league || league=="all"){
-    let liveGame = fixture.scores.length != 0 && fixture.result_info === null ? true : false;
-    let finished = fixture.result_info !== null ? true : false;
-    let fixtureData = encodeURIComponent(JSON.stringify(fixture));
-    let currentFixture = document.createElement('tr');
-    let matchup = document.createElement('td');
-    let teams = fixture.name.split(' vs ');
-    teams.forEach(team => { let teamspan = document.createElement('p'); teamspan.innerText = team; matchup.appendChild(teamspan); });
-
-    let scores = document.createElement('td');
-    if (liveGame || finished) {
-
-    }
-
-    let button = document.createElement('td');
-    let buttonlink = document.createElement('a');  buttonlink.setAttribute('class', 'bg-[#1bb198] border-t-0 border-y-4 border-green-800 hover:bg-green-700 text-white font-bold my-3 p-2 text-center w-full rounded shadow-slate-900  shadow-md hover:shadow-green-500/50');
-    
-    
-    button.appendChild(buttonlink);
-
-    let league = document.createElement('td');
-    let leaguename = document.createElement('p'); leaguename.innerText = fixture.league.name;
-    league.appendChild(leaguename);
-
-    let date = document.createElement('td'); date.setAttribute('class', 'flex flex-1 m-3 flex-col text-right');
-    let datestamp = document.createElement('p'); datestamp.innerText = fixture.starting_at.split(" ")[0];
-    date.appendChild(datestamp);
-
-    currentFixture.appendChild(matchup);
-    if(liveGame || finished){
-      fixture.scores.forEach(el => {
-        if (el.description == "CURRENT") {
-          if (el.score.participant == "home") {
-            let homescore = document.createElement('p'); homescore.innerText = String(el.score.goals);
-            scores.appendChild(homescore);
-          }
-          if (el.score.participant == "away") {
-            let awayscore = document.createElement('p'); awayscore.innerText = String(el.score.goals);
-            scores.appendChild(awayscore);
-          }
-        }
+  fixtures.forEach((fixture) => {
+    if (fixture.league_id == league || league == "all") {
+      let liveGame =
+        fixture.scores.length != 0 && fixture.result_info === null
+          ? true
+          : false;
+      let finished = fixture.result_info !== null ? true : false;
+      let fixtureData = encodeURIComponent(JSON.stringify(fixture));
+      let currentFixture = document.createElement("tr");
+      let matchup = document.createElement("td");
+      let teams = fixture.name.split(" vs ");
+      teams.forEach((team) => {
+        let teamspan = document.createElement("p");
+        teamspan.innerText = team;
+        matchup.appendChild(teamspan);
       });
-      currentFixture.appendChild(scores);
-      let possesionEl = document.createElement('td');
-      let possesionTextH = document.createElement('p');
-      let possesionTextA = document.createElement('p');
-      let possesionStats = fixture.statistics.filter((el) => el.type.code == "ball-possession" ? true : false)
-      console.log(possesionStats)
-      if (possesionStats.length!=0){
-        possesionTextH.innerText = possesionStats.filter((el) => el.location == "home" ? true : false)[0].data.value+'%';
-        possesionTextA.innerText = possesionStats.filter((el) => el.location == "away" ? true : false)[0].data.value+'%';
-        possesionEl.appendChild(possesionTextH); possesionEl.appendChild(possesionTextA);
+
+      let scores = document.createElement("td");
+      if (liveGame || finished) {
       }
 
-      currentFixture.appendChild(possesionEl)
-      buttonlink.innerText = 'More info';
-      buttonlink.setAttribute('href', `info.html?data=${fixture.id}`)
-      currentFixture.appendChild(button);
-    } else{
-      buttonlink.innerText = 'Predict';
-      buttonlink.setAttribute('href', `simulator.html?data=${fixtureData}`)
-      currentFixture.appendChild(button);
+      let button = document.createElement("td");
+      let buttonlink = document.createElement("a");
+      buttonlink.setAttribute(
+        "class",
+        "bg-[#1bb198] border-t-0 border-y-4 border-green-800 hover:bg-green-700 text-white font-bold my-3 p-2 text-center w-full rounded shadow-slate-900  shadow-md hover:shadow-green-500/50"
+      );
+
+      button.appendChild(buttonlink);
+
+      let league = document.createElement("td");
+      let leaguename = document.createElement("p");
+      leaguename.innerText = fixture.league.name;
+      league.appendChild(leaguename);
+
+      let date = document.createElement("td");
+      date.setAttribute("class", "flex flex-1 m-3 flex-col text-right");
+      let datestamp = document.createElement("p");
+      datestamp.innerText = fixture.starting_at.split(" ")[0];
+      date.appendChild(datestamp);
+
+      currentFixture.appendChild(matchup);
+      if (liveGame || finished) {
+        fixture.scores.forEach((el) => {
+          if (el.description == "CURRENT") {
+            if (el.score.participant == "home") {
+              let homescore = document.createElement("p");
+              homescore.innerText = String(el.score.goals);
+              scores.appendChild(homescore);
+            }
+            if (el.score.participant == "away") {
+              let awayscore = document.createElement("p");
+              awayscore.innerText = String(el.score.goals);
+              scores.appendChild(awayscore);
+            }
+          }
+        });
+        currentFixture.appendChild(scores);
+        let possesionEl = document.createElement("td");
+        let possesionTextH = document.createElement("p");
+        let possesionTextA = document.createElement("p");
+        let possesionStats = fixture.statistics.filter((el) =>
+          el.type.code == "ball-possession" ? true : false
+        );
+        console.log(possesionStats);
+        if (possesionStats.length != 0) {
+          possesionTextH.innerText =
+            possesionStats.filter((el) =>
+              el.location == "home" ? true : false
+            )[0].data.value + "%";
+          possesionTextA.innerText =
+            possesionStats.filter((el) =>
+              el.location == "away" ? true : false
+            )[0].data.value + "%";
+          possesionEl.appendChild(possesionTextH);
+          possesionEl.appendChild(possesionTextA);
+        }
+
+        currentFixture.appendChild(possesionEl);
+        buttonlink.innerText = "More info";
+        buttonlink.setAttribute("href", `info.html?data=${fixture.id}`);
+        currentFixture.appendChild(button);
+      } else {
+        buttonlink.innerText = "Predict";
+        buttonlink.setAttribute("href", `simulator.html?data=${fixtureData}`);
+        currentFixture.appendChild(button);
+      }
+      currentFixture.appendChild(league);
+      currentFixture.appendChild(date);
+
+      if (liveGame) {
+        liveGamesContainer.appendChild(currentFixture);
+      } else if (finished) {
+        finishedGamesContainer.appendChild(currentFixture);
+      } else {
+        schedualedGamesContainer.appendChild(currentFixture);
+      }
     }
-    currentFixture.appendChild(league);
-    currentFixture.appendChild(date);
-
-    if(liveGame){ liveGamesContainer.appendChild(currentFixture);} 
-    else if(finished){finishedGamesContainer.appendChild(currentFixture);} 
-    else{schedualedGamesContainer.appendChild(currentFixture);}
-    
-  }});
+  });
 }
 
-function DateChanged(e){
-  let date = e.target.value.split('-');
-  let league = document.getElementById('leagueSelect').value
-  liveGamesContainer.innerHTML="";
-  schedualedGamesContainer.innerHTML="";
-  finishedGamesContainer.innerHTML="";
+function DateChanged(e) {
+  let date = e.target.value.split("-");
+  let league = document.getElementById("leagueSelect").value;
+  liveGamesContainer.innerHTML = "";
+  schedualedGamesContainer.innerHTML = "";
+  finishedGamesContainer.innerHTML = "";
 
-  GetMatchups(date[0],date[1],date[2],1,league);
+  GetMatchups(date[0], date[1], date[2], 1, league);
 }
 
-fetch(
-  `../src/data/leaguelist.json`
-)
+fetch(`../src/data/leaguelist.json`)
   .then((response) => response.json())
   .then((data) => GetAllLeagues(data.data));
 function GetAllLeagues(data) {
@@ -144,11 +173,11 @@ function GetAllLeagues(data) {
   }
 }
 
-function LeagueChanged(event){
-  let fixturesSelDate = JSON.parse(sessionStorage.getItem('SHOWNMATCHUPS'))
-  liveGamesContainer.innerHTML="";
-  schedualedGamesContainer.innerHTML="";
-  finishedGamesContainer.innerHTML="";
-  ParseMatchups(fixturesSelDate, event.target.value)
-  console.log(fixturesSelDate)
+function LeagueChanged(event) {
+  let fixturesSelDate = JSON.parse(sessionStorage.getItem("SHOWNMATCHUPS"));
+  liveGamesContainer.innerHTML = "";
+  schedualedGamesContainer.innerHTML = "";
+  finishedGamesContainer.innerHTML = "";
+  ParseMatchups(fixturesSelDate, event.target.value);
+  console.log(fixturesSelDate);
 }
